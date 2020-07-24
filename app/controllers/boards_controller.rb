be2dffcef2c0f3e67d05c6cf0cc49e1efa510056
Kelsey.Chen <naoyakaoru@gmail.com>
2020-07-24 10:19:38 +0800

    Day 1 progress

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,10 +1,13 @@
 class BoardsController < ApplicationController
-def index
-  @boards = Board.all
-end
-def new
-end
-def create
-  render html: params
-end
+  def index
+    @boards = Board.all
+  end
+
+  def new
+  end
+
+  def create
+    Board.create(title: params[:title], intro: params[:intro])
+    render html: "ok"
+  end
 end
