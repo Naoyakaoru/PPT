4a3330ee1a7b4c7e7cde81f042c128d42a73e442
Kelsey.Chen <naoyakaoru@gmail.com>
2020-08-03 15:32:35 +0800

    more methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,13 +10,11 @@ class ApplicationController < ActionController::Base
   #   end
   # end
   def user_signed_in? #讓每個方法只做一件事就好，本方法只檢查是否有登入
-    session[:user_token]
+    current_user != nil
   end
   
   def current_user #有登入的話就可以執行取資料
-    if user_signed_in?
-      User.find(session[:user_token])
-    end
+    @current_user ||= User.find_by(id: session[:user_token])
   end
 
   def not_found
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,16 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if  @user.save
       #登入
-      session[:user_token] = @user.id #id或account都可以，但注意是不可重複的
+      session[:user_token] = @user.id #id或account都可以，但注意是不可重複的 #這邊是在做登入狀況的設定
       redirect_to root_path, notice: '會員註冊成功' #去首頁
     else
      render :sign_up
     end
+  end
+
+  def sign_out
+    session[:user_token] = nil
+    redirect_to root_path, notice: "登出成功"
   end
 
   private
