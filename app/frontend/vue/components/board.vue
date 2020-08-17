11d72cba8367afe62f7c593b6a66cd6260475054
Kelsey.Chen <naoyakaoru@gmail.com>
2020-08-17 10:49:52 +0800

    aasm

--- app/frontend/vue/components/board.vue
+++ app/frontend/vue/components/board.vue
@@ -1,19 +1,35 @@
 <template>
   <div>
-    <li v-for="board in boards">{{ board }}</li>
+    <ul>
+      <li v-for="board in boards" :key="board.id">
+        <a :href="'/boards/' + board.id">{{ board.title }}</a>
+        <a :href="'/boards/' + board.id" data-confirm="確定刪除？" data-method="delete">刪除</a>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import R from "@rails/ujs";
+
 export default {
   data: function () {
     return {
       boards: [],
     };
   },
+  beforeCreate: function () {
+    R.ajax({
+      url: "/api/v1/boards.json",
+      type: "GET",
+      success: (data) => {
+        this.boards = data;
+      },
+      error: function (err) {},
+    });
+  },
 };
 </script>
 
 <style scoped>
-  
 </style>
